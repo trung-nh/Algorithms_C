422eed1d01ae43e65c044e369b6ebf7ab899467a
hoantrung112 <nguyenhoantrung.1102@gmail.com>
2021-01-07 14:21:22 +0700

    added: drone pickup

---
+++ Drone Pickup/drone.c
@@ -0,0 +1,12 @@
+// https://codeforces.com/group/Ir5CI6f3FD/contest/276073/problem/L
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n, K;
+    FILE *fptr = fopen("input.txt", "r");
+    scanf(fptr, "%d%d", &n, &K);
+    int goods[n], energy[n];
+    return 0;
+}
